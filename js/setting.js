16f46ec5d9b6b7145975694e1d542c1108d2b80a
Yusei Ito <yuseikun@outlook.jp>
2018-09-02 10:41:52 +0900

    category count changed

--- js/setting.js
+++ js/setting.js
@@ -1,11 +1,11 @@
 /*
  * 5374 setting
  *
-*/
+ */
 
 var SVGLabel = true; // SVGイメージを使用するときは、true。用意できない場合はfalse。
 
-var MaxDescription = 9; // ごみの最大種類、９を超えない場合は変更の必要はありません。
+var MaxDescription = 15; // ごみの最大種類、９を超えない場合は変更の必要はありません。
 
 var MaxMonth = 3;
 
